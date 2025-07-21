a9de2e4dc90f1aef1e3b8906c2320ecc8cb44775
bimaputra86 <bimajayanusa@gmail.com>
2025-07-21 20:29:06 +0700

    barang controller show bima

--- app/Http/Controllers/API/BarangController.php
+++ app/Http/Controllers/API/BarangController.php
@@ -30,5 +30,21 @@ class BarangController extends Controller
      * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-   
+    public function show($id)
+    {
+        $barang = Barang::find($id);
+        
+        if (!$barang) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Data barang tidak ditemukan'
+            ], 404);
+        }
+        
+        return response()->json([
+            'success' => true,
+            'message' => 'Detail data barang',
+            'data' => $barang
+        ]);
+    }
 } 
